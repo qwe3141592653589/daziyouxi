af1cd1ea0fe93468988007030c44365484e1aef1
qwe3141592653589 <165156154+qwe3141592653589@users.noreply.github.com>
2026-02-08 10:22:07 +0000

    优化了些许代码

--- daziyouxi_v1.2.py
+++ daziyouxi_v1.2.py
@@ -7,12 +7,13 @@
 # m1 = 键入的类型，类型见生成器类型
 # m2 = 键入的模式，1 = 有限生成 2 = 无限生成
 # r = 返回内容
-# e = 错误数量
+# e = 错误数量)
 # t = 正确数量
 # m2_sys 有限生成数量
 # temp_r 临时数据-用户游戏开始时输入的内容
 # everynum 用户答题总数
 # help 帮助文档
+# temp3 导入询问
 
 #预定义
 ABC_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
@@ -116,67 +117,65 @@ def abc(a):
             return temp1
 
 #主程序
-#转译类型参数
-if m1 == '1':
-    m1 = 'a'
-elif m1 == '2':
-    m1 = 'A'
-elif m1 == '3':
-    m1 = '1'
-elif m1 == '4':
-    m1 = 'A1'
-elif m1 == '5':
-    m1 = 'a1'
-elif m1 == '6':
-    m1 = 'Aa1'
-elif m1 == '7':
-    m1 = 'Aa'
-#输出并判断
-print('游戏开始')
-if m2 == '2':
-    while True:
-        r = abc(m1)
-        r = str(r)
-        temp_r = input(r)
-        if 'exit' == str(temp_r):
-            print('你打了',everynum,'个字母或数字,其中正确:',t,'错误:',e)
-            print('游戏结束')
-            if e > t:
-                for i in range(5):
-                    print('菜！就多练，以前是以前，现在是现在！')
-            sys.exit()
-        if r == str(temp_r):
-            t += 1
-            everynum += 1
-            print('正确','目前正确数量:',t,'错误数量:',e)
-        else:
-            e += 1
-            everynum += 1
-            print('错误','目前正确数量:',t,'错误数量:',e)
-elif m2 == '1':
-    print('目前是有限模式，当设定的数量达到时自动退出，提前退出请键入exit','设定量为:',m2_sys)
-    for i in range(int(m2_sys)):
-        r = abc(m1)
-        temp_r = input(r)
-        if 'exit' == str(temp_r):
-            print('你打了',everynum,'个字母或数字,其中正确:',t,'错误:',e,'剩余:',int(m2_sys)-everynum,'总量:',m2_sys)
-            print('游戏结束')
-            if t < e:
-                for i in range(5):
-                    print('菜！就多练，以前是以前，现在是现在！')
-            sys.exit()
-        if str(r) == str(temp_r):
-            t += 1
-            everynum += 1
-            print('正确','目前正确数量:',t,'错误数量:',e,'已打:',everynum,'剩余:',int(m2_sys)-everynum,'总量:',m2_sys)
-        else:
-            e += 1
-            everynum += 1
-            print('错误','目前正确数量:',t,'错误数量:',e,'已打:',everynum,'剩余:',int(m2_sys)-everynum,'总量:',m2_sys)
-    print('恭喜！完成了目标','已打:',everynum,'总量:',m2_sys,'正确数量:',t,'错误数量:',e)
-    if t < e:
-        for i in range(5):
-            print('菜！就多练，以前是以前，现在是现在！')
-else:
-    print('似乎没有运行成功，请重试')
+def game():
+    if m1 == '1':
+        m1 = 'a'
+    elif m1 == '2':
+        m1 = 'A'
+    elif m1 == '3':
+        m1 = '1'
+    elif m1 == '4':
+        m1 = 'A1'
+    elif m1 == '5':
+        m1 = 'a1'
+    elif m1 == '6':
+        m1 = 'Aa1'
+    elif m1 == '7':
+        m1 = 'Aa'
+    global m2;m1;a;ABC_list;abc_list;temp1;temp2;r;e;t;m2_sys;everynum;help
+    print('游戏开始')
+    if m2 == '2':
+        while True:
+            r = abc(m1)
+            r = str(r)
+            temp_r = input(r)
+            if 'exit' == str(temp_r):
+                print('你打了',everynum,'个字母或数字,其中正确:',t,'错误:',e)
+                print('游戏结束')
+                if e > t:
+                    for i in range(5):
+                        print('菜！就多练，以前是以前，现在是现在！')
+                sys.exit()
+            if r == str(temp_r):
+                t += 1
+                everynum += 1
+                print('正确','目前正确数量:',t,'错误数量:',e)
+            else:
+                e += 1
+                everynum += 1
+                print('错误','目前正确数量:',t,'错误数量:',e)
+    elif m2 == '1':
+        print('目前是有限模式，当设定的数量达到时自动退出，提前退出请键入exit','设定量为:',m2_sys)
+        for i in range(int(m2_sys)):
+            r = abc(m1)
+            temp_r = input(r)
+            if 'exit' == str(temp_r):
+                print('你打了',everynum,'个字母或数字,其中正确:',t,'错误:',e,'剩余:',int(m2_sys)-everynum,'总量:',m2_sys)
+                print('游戏结束')
+                if t < e:
+                    for i in range(5):
+                        print('菜！就多练，以前是以前，现在是现在！')
+                sys.exit()
+            if str(r) == str(temp_r):
+                t += 1
+                everynum += 1
+                print('正确','目前正确数量:',t,'错误数量:',e,'已打:',everynum,'剩余:',int(m2_sys)-everynum,'总量:',m2_sys)
+            else:
+                e += 1
+                everynum += 1
+                print('错误','目前正确数量:',t,'错误数量:',e,'已打:',everynum,'剩余:',int(m2_sys)-everynum,'总量:',m2_sys)
+        print('恭喜！完成了目标','已打:',everynum,'总量:',m2_sys,'正确数量:',t,'错误数量:',e)
+        if t < e:
+            for i in range(5):
+                print('菜！就多练，以前是以前，现在是现在！')
 print('运行结束')
